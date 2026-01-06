a61af7b5ce34a5f6bfe5e554740e606b7f74350a
Jhonata Amboni <jhonata01amboni@gmail.com>
2026-01-06 19:25:52 -0300

    adicionando testes unitarios ao controlador do client

--- src/test/java/com/meiorganizadinho/controller/ClientControllerTest.java
+++ src/test/java/com/meiorganizadinho/controller/ClientControllerTest.java
@@ -1,17 +1,28 @@
 package com.meiorganizadinho.controller;
 
+import com.meiorganizadinho.config.UserAuthenticationFilter;
+import com.meiorganizadinho.repository.UserRepository;
 import com.meiorganizadinho.service.ClientService;
+import com.meiorganizadinho.service.JwtTokenService;
+import com.meiorganizadinho.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ClientController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc(addFilters = false)
 public class ClientControllerTest {
 
     @Autowired
@@ -20,8 +31,18 @@ public class ClientControllerTest {
     @MockitoBean
     private ClientService clientService;
 
+    @MockitoBean
+    private UserAuthenticationFilter userAuthenticationFilter;
+
+    @MockitoBean
+    private JwtTokenService jwtTokenService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
     @Test
-    void shouldReturn400WhenNameIsEmpty() throws Exception {
+    @WithMockUser
+    void postShouldReturn400WhenNameIsEmpty() throws Exception {
         String jsonRequest = """
                 {
                     "name": ""
@@ -31,6 +52,42 @@ public class ClientControllerTest {
         mockMvc.perform(post("/api/v1/clients")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonRequest))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Bad Request"))
+                .andExpect(jsonPath("$.details").value("Name is required"));
+    }
+
+    @Test
+    @WithMockUser
+    void postShouldReturn400WhenNameIsNull() throws Exception {
+        String jsonRequest = """
+                {
+                    "name":
+                }
+                """;
+
+        mockMvc.perform(post("/api/v1/clients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Bad Request"))
+                .andExpect(jsonPath("$.details").value("Invalid request body"));
+    }
+
+    @Test
+    @WithMockUser
+    void postShouldReturn400WhenNameDoesnotExists() throws Exception {
+        String jsonRequest = """
+                {
+                    "ame": ""
+                }
+                """;
+
+        mockMvc.perform(post("/api/v1/clients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Bad Request"))
+                .andExpect(jsonPath("$.details").value("Invalid request body"));
     }
 }
