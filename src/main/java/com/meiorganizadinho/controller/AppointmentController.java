60171979fe1c4520778307afe1634df4493df98f
Jhonata Amboni <jhonata01amboni@gmail.com>
2026-02-07 19:21:19 -0300

    adjust unit test

--- src/main/java/com/meiorganizadinho/controller/AppointmentController.java
+++ src/main/java/com/meiorganizadinho/controller/AppointmentController.java
@@ -45,7 +45,6 @@ public class AppointmentController {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         appointmentService.delete(id);
 
-        //return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    } //
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
